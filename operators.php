df6ce75e1f10a7f6b2308c3b6fab2b226b375a7e
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-07-30 21:16:22 -0600

    Create operators.php

---
+++ operators.php
@@ -0,0 +1,10 @@
+<?php
+    //Multiply
+    echo 5 * 3;
+    //Divide
+    echo 10/3;
+    //comparison operator to check if $a is equal to $b.
+    var_dump($a==$b);
+    //comparison operator to check if $a is NOT equal to $b.
+    var_dump($a!=$b);
+?>
