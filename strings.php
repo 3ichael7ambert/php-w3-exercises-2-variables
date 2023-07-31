5f13a9b1d6c37e17c8f49d7622bf5d78f737bda1
3ichael 7ambert <mlambertbusiness@gmail.com>
2023-07-30 21:12:50 -0600

    Create strings.php

---
+++ strings.php
@@ -0,0 +1,9 @@
+<?php
+    echo strlen("Hello World!"); //returns length of the string
+    echo strrev("Hello World!"); //returns the string reversed
+
+
+    
+    $oldtxt = "Hello World!";
+    $newtxt = str_replace("World","Dolly",$oldtxt);
+?>
